bc964c38f8ccc479c9376e344a93602deccd7da8
hendisantika <hendisantika@yahoo.co.id>
2025-06-18 07:45:27 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/fileuploaddownload/SpringBootFileuploadDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.fileuploaddownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFileuploadDownloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
