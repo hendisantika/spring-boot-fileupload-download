e44c4333213510ca009a70bebb7853ad5931fcb8
hendisantika <hendisantika@yahoo.co.id>
2025-06-18 07:51:32 +0700

    feat: Add downloadFile in FileProcessingController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/fileuploaddownload/controller/FileProcessingController.java
+++ src/main/java/id/my/hendisantika/fileuploaddownload/controller/FileProcessingController.java
@@ -2,10 +2,13 @@ package id.my.hendisantika.fileuploaddownload.controller;
 
 import id.my.hendisantika.fileuploaddownload.service.FileProcessingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -29,5 +32,15 @@ public class FileProcessingController {
     @GetMapping("/list")
     public ResponseEntity<?> getFileList() {
         return new ResponseEntity<>(fileProcessingService.fileList(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/download/{name}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<?> downloadFile(@PathVariable(value = "name") String fileName) {
+        Resource file = fileProcessingService.downloadFile(fileName);
+        if (file == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM).body(file);
+        }
     }
 }
