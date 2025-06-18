66589e185119e4a3895aded37be7fa12f8aed29d
hendisantika <hendisantika@yahoo.co.id>
2025-06-18 07:48:15 +0700

    feat: Add uploadFile in FileProcessingService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/fileuploaddownload/service/FileProcessingService.java
+++ src/main/java/id/my/hendisantika/fileuploaddownload/service/FileProcessingService.java
@@ -2,8 +2,12 @@ package id.my.hendisantika.fileuploaddownload.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,5 +33,23 @@ public class FileProcessingService {
         File[] files = dir.listFiles();
 
         return files != null ? Arrays.stream(files).map(File::getName).collect(Collectors.toList()) : null;
+    }
+
+    public String uploadFile(MultipartFile multipartFile) {
+        File dir = new File(basePath + multipartFile.getName());
+
+        if (dir.exists()) {
+            return "EXIST";
+        }
+
+        Path path = Path.of(basePath + multipartFile.getName());
+
+        try {
+            Files.copy(multipartFile.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+            return "CREATED";
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return "FAILED";
     }
 }
