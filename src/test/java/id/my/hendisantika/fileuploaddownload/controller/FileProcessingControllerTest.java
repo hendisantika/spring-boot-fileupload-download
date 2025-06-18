90cd4a5dea0b3323c9ddf9ab9d54f1cef16bdcc7
hendisantika <hendisantika@yahoo.co.id>
2025-06-18 07:54:30 +0700

    feat: Add getAllFilesShouldReturnExistingFileList🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/fileuploaddownload/controller/FileProcessingControllerTest.java
+++ src/test/java/id/my/hendisantika/fileuploaddownload/controller/FileProcessingControllerTest.java
@@ -1,11 +1,21 @@
 package id.my.hendisantika.fileuploaddownload.controller;
 
 import id.my.hendisantika.fileuploaddownload.service.FileProcessingService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,4 +42,15 @@ class FileProcessingControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Test
+    public void getAllFilesShouldReturnExistingFileList() throws Exception {
+        List<String> files = List.of("file1.txt", "file2.txt");
+
+        given(service.fileList()).willReturn(files);
+
+        mvc.perform(get("/file/list")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
 }
