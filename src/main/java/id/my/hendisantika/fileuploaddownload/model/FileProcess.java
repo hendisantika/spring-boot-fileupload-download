ebec4b8a1e65d78c60eb9d97f20f111c1835df76
hendisantika <hendisantika@yahoo.co.id>
2025-06-18 07:46:53 +0700

    feat: Add FileProcess Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/fileuploaddownload/model/FileProcess.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.fileuploaddownload.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-fileupload-download
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 18/06/25
+ * Time: 07.46
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class FileProcess {
+    String fileName;
+    String fileContent;
+}
